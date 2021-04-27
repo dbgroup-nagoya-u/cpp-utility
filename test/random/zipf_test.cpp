5783e1100929658370ce4cc8f08665efa10c6c5b
Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>
2021-04-27 15:58:01 +0900

    add test to check class correctly simulate skew

--- test/random/zipf_test.cpp
+++ test/random/zipf_test.cpp
@@ -5,14 +5,30 @@
 
 #include <gtest/gtest.h>
 
+#include <cmath>
+
 namespace dbgroup::random::zipf
 {
 class ZipfGeneratorFixture : public ::testing::Test
 {
  public:
   static constexpr size_t kRepeatNum = 10000;
+  static constexpr size_t kBinNum = 100;
+  static constexpr double kAllowableError = 0.01;
 
-  ZipfGenerator zipf_gen;
+  void
+  CheckProbsObeyZipfLaw(  //
+      const std::vector<size_t> &freq_dist,
+      const double alpha)
+  {
+    const auto base_prob = static_cast<double>(freq_dist[0]) / kRepeatNum;
+    for (size_t k = 2; k <= kBinNum; ++k) {
+      const auto kth_prob = static_cast<double>(freq_dist[k - 1]) / kRepeatNum;
+      const auto error = abs(kth_prob - base_prob / pow(k, alpha));
+
+      EXPECT_LT(error, kAllowableError);
+    }
+  }
 
  protected:
   void
@@ -30,13 +46,32 @@ class ZipfGeneratorFixture : public ::testing::Test
  * Public utility tests
  *------------------------------------------------------------------------------------------------*/
 
-TEST_F(ZipfGeneratorFixture, Construct_WithoutArgs_MemberVariablesCorrectlyInitialized)
+TEST_F(ZipfGeneratorFixture, Construct_WithoutArgs_ZipfGenerateAlwaysZero)
 {
-  zipf_gen = ZipfGenerator{};
+  ZipfGenerator zipf_gen{};
 
   for (size_t i = 0; i < kRepeatNum; ++i) {
     const auto zipf_val = zipf_gen.Zipf();
     EXPECT_EQ(zipf_val, 0);
+  }
+}
+
+TEST_F(ZipfGeneratorFixture, Construct_WithArgs_ZipfGenerateCorrectSkewVal)
+{
+  for (double alpha = 0; alpha < 2; alpha += 0.1) {
+    ZipfGenerator zipf_gen{kBinNum, alpha, 0};
+
+    std::vector<size_t> freq_dist(kBinNum, 0);
+    for (size_t i = 0; i < kRepeatNum; ++i) {
+      const auto zipf_val = zipf_gen.Zipf();
+
+      ASSERT_GE(zipf_val, 0);
+      ASSERT_LT(zipf_val, kBinNum);
+
+      ++freq_dist[zipf_val];
+    }
+
+    CheckProbsObeyZipfLaw(freq_dist, alpha);
   }
 }
 
