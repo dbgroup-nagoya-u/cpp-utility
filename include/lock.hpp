06e3a0c555cae62f47b6b2881c1d902cb92882ee
Kenta Nohara <53135351+nrkt@users.noreply.github.com>
2022-07-01 01:52:35 +0900

    Update include/lock.hpp
    Co-authored-by: Kento Sugiura <sugiura@db.is.i.nagoya-u.ac.jp>

--- include/lock.hpp
+++ include/lock.hpp
@@ -58,10 +58,10 @@ class PessimisticLock
   void
   LockShared()
   {
-    auto expected = lock_ & (~1);  // turn-off LSB
+    auto expected = lock_.load(std::memory_order_acquire) & (~1UL);  // turn-off LSB
     auto desired = expected + 2;   // increment read-counter
-    while (!lock_.compare_exchange_strong(expected, desired, std::memory_order_relaxed)) {
-      expected = expected & (~1);
+    while (!lock_.compare_exchange_strong(expected, desired, std::memory_order_relaxed, std::memory_order_acquire)) {
+      expected = expected & (~1UL);
       desired = expected + 2;
       SPINLOCK_HINT
     }
