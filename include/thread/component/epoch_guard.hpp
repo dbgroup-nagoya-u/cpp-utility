9ccc2201193bcfd07c6447df9a8c6f04bbc4cedf
Kento Sugiura <sugiura@i.nagoya-u.ac.jp>
2024-03-13 17:10:05 +0900

    add EpochGuard class

---
+++ include/thread/component/epoch_guard.hpp
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2024 Database Group, Nagoya University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DBGROUP_THREAD_COMPONENT_EPOCH_GUARD_HPP
+#define DBGROUP_THREAD_COMPONENT_EPOCH_GUARD_HPP
+
+// local sources
+#include "thread/component/epoch.hpp"
+
+namespace dbgroup::thread::component
+{
+/**
+ * @brief A class to protect epochs based on the scoped locking pattern.
+ *
+ */
+class EpochGuard
+{
+ public:
+  /*############################################################################
+   * Public constructors and assignment operators
+   *##########################################################################*/
+
+  constexpr EpochGuard() = default;
+
+  /**
+   * @brief Construct a new instance and protect a current epoch.
+   *
+   * @param epoch A reference to a target epoch.
+   */
+  explicit EpochGuard(  //
+      Epoch *epoch);
+
+  /**
+   * @brief Construct a new instance.
+   *
+   * @param obj An rvalue reference.
+   */
+  constexpr EpochGuard(  //
+      EpochGuard &&obj) noexcept
+  {
+    epoch_ = obj.epoch_;
+    obj.epoch_ = nullptr;
+  }
+
+  /**
+   * @brief Construct a new instance.
+   *
+   * @param obj An rvalue reference.
+   */
+  constexpr auto
+  operator=(                      //
+      EpochGuard &&obj) noexcept  //
+      -> EpochGuard &
+  {
+    epoch_ = obj.epoch_;
+    obj.epoch_ = nullptr;
+    return *this;
+  }
+
+  // delete the copy constructor/assignment
+  EpochGuard(const EpochGuard &) = delete;
+  auto operator=(const EpochGuard &) -> EpochGuard & = delete;
+
+  /*############################################################################
+   * Public destructors
+   *##########################################################################*/
+
+  /**
+   * @brief Destroy the instace and release a protected epoch.
+   *
+   */
+  ~EpochGuard();
+
+  /*############################################################################
+   * Public getters
+   *##########################################################################*/
+
+  /**
+   * @return The epoch value protected by this object.
+   */
+  [[nodiscard]] auto GetProtectedEpoch() const  //
+      -> size_t;
+
+ private:
+  /*############################################################################
+   * Internal member variables
+   *##########################################################################*/
+
+  /// @brief A reference to a target epoch.
+  Epoch *epoch_{nullptr};
+};
+
+}  // namespace dbgroup::thread::component
+
+#endif  // DBGROUP_THREAD_COMPONENT_EPOCH_GUARD_HPP
---
+++ test/thread/epoch_guard_test.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2021 Database Group, Nagoya University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// the correspnding header
+#include "thread/component/epoch_guard.hpp"
+
+// C++ standard libraries
+#include <atomic>
+#include <cstddef>
+#include <limits>
+#include <memory>
+
+// external libraries
+#include "gtest/gtest.h"
+
+// library headers
+#include "thread/component/epoch.hpp"
+
+namespace dbgroup::thread::component::test
+{
+class EpochGuardFixture : public ::testing::Test
+{
+ protected:
+  /*############################################################################
+   * Constants
+   *##########################################################################*/
+
+  static constexpr size_t kULMax = std::numeric_limits<size_t>::max();
+
+  /*############################################################################
+   * Test setup/teardown
+   *##########################################################################*/
+
+  void
+  SetUp() override
+  {
+    current_epoch_ = 0;
+    epoch_ = std::make_unique<Epoch>();
+    epoch_->SetGrobalEpoch(&current_epoch_);
+  }
+
+  void
+  TearDown() override
+  {
+  }
+
+  /*############################################################################
+   * Internal member variables
+   *##########################################################################*/
+
+  std::atomic_size_t current_epoch_{};
+
+  std::unique_ptr<Epoch> epoch_{nullptr};
+};
+
+/*##############################################################################
+ * Unit test definitions
+ *############################################################################*/
+
+TEST_F(EpochGuardFixture, ConstructorWithCurrentEpochProtectEpoch)
+{
+  const EpochGuard guard{epoch_.get()};
+
+  EXPECT_EQ(0, epoch_->GetProtectedEpoch());
+}
+
+TEST_F(EpochGuardFixture, DestructorWithCurrentEpochUnprotectEpoch)
+{
+  {
+    const EpochGuard guard{epoch_.get()};
+  }
+
+  EXPECT_EQ(kULMax, epoch_->GetProtectedEpoch());
+}
+
+TEST_F(EpochGuardFixture, MoveConstructorKeepEpochProtected)
+{
+  EpochGuard guard{epoch_.get()};
+  [[maybe_unused]] EpochGuard moved{std::move(guard)};
+
+  EXPECT_EQ(0, epoch_->GetProtectedEpoch());
+}
+
+TEST_F(EpochGuardFixture, MoveAssignmentKeepEpochProtected)
+{
+  EpochGuard guard{epoch_.get()};
+  [[maybe_unused]] auto &&moved = std::move(guard);
+
+  EXPECT_EQ(0, epoch_->GetProtectedEpoch());
+}
+
+}  // namespace dbgroup::thread::component::test
