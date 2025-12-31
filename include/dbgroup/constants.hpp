939d49ef7e05f0c37dde9a09680ad03f3b8ff9bc
Kento Sugiura <sugiura@i.nagoya-u.ac.jp>
2025-12-31 12:30:33 +0900

    add power of two constants

--- include/dbgroup/constants.hpp
+++ include/dbgroup/constants.hpp
@@ -27,6 +27,54 @@ namespace dbgroup
  * Global constants
  *############################################################################*/
 
+/// @brief 2
+constexpr size_t k2 = 1UL << 1UL;
+
+/// @brief 4
+constexpr size_t k4 = 1UL << 2UL;
+
+/// @brief 8
+constexpr size_t k8 = 1UL << 3UL;
+
+/// @brief 16
+constexpr size_t k16 = 1UL << 4UL;
+
+/// @brief 32
+constexpr size_t k32 = 1UL << 5UL;
+
+/// @brief 64
+constexpr size_t k64 = 1UL << 6UL;
+
+/// @brief 128
+constexpr size_t k128 = 1UL << 7UL;
+
+/// @brief 256
+constexpr size_t k256 = 1UL << 8UL;
+
+/// @brief 512
+constexpr size_t k512 = 1UL << 9UL;
+
+/// @brief 1,024
+constexpr size_t k1Ki = 1UL << 10UL;
+
+/// @brief 2,048
+constexpr size_t k2Ki = 1UL << 11UL;
+
+/// @brief 4,096
+constexpr size_t k4Ki = 1UL << 12UL;
+
+/// @brief 8,192
+constexpr size_t k8Ki = 1UL << 13UL;
+
+/// @brief 16,384
+constexpr size_t k16Ki = 1UL << 14UL;
+
+/// @brief 32,768
+constexpr size_t k32Ki = 1UL << 15UL;
+
+/// @brief 65,536
+constexpr size_t k64Ki = 1UL << 16UL;
+
 /// @brief An alias of the sequential-consistency memory order.
 constexpr std::memory_order kSeqCst = std::memory_order_seq_cst;
 
