c7fff5ef6ac895c26919e336faf2b11680ca0e97
Seigo Ai <107398315+sigu03@users.noreply.github.com>
2026-08-13 15:46:52 +0900

    Update src/lock/omcs_lock.cpp
    Co-authored-by: Kento Sugiura <sugiura@i.nagoya-u.ac.jp>

--- src/lock/omcs_lock.cpp
+++ src/lock/omcs_lock.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Database Group, Nagoya University
+ * Copyright 2026 Database Group, Nagoya University
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
